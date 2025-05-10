3355cb6787563d2a4ee9cec965cc4eedc015dcb1
Maeght Loan <37497007+loan-mgt@users.noreply.github.com>
2025-05-10 18:19:52 +0000

    make request

--- public/main.js
+++ public/main.js
@@ -11,6 +11,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 option.textContent = civ.name;
                 select.appendChild(option);
             });
+            select.addEventListener('change', (e) => {
+                const civCode = e.target.value;
+                if (civCode) {
+                    fetch(`${api}/civilisations/${civCode}/units`)
+                        .then(res => res.json())
+                        .then(units => {
+                            // TODO: Render units in the units-list section
+                            console.log('Units:', units);
+                        })
+                        .catch(err => {
+                            console.error('Failed to load units:', err);
+                        });
+                } else {
+                    // TODO: Clear units-list section
+                }
+            });
         })
         .catch(err => {
             console.error('Failed to load civilisations:', err);
